c1895d0f2ddc03d8e581f01ec3a9663c2bc509ce
YellowSegment <yellowsegment66@gmail.com>
2023-04-26 19:28:37 -0400

    Added Player model and other
    Added a player model and limited the rotation of the camera. However, I did somehow break the crouch mechanic, I assume with the new character and the collision mesh. Fixing is soon

--- PaintSim/Assets/PlayerCam.cs
+++ PaintSim/Assets/PlayerCam.cs
@@ -27,7 +27,7 @@ public class PlayerCam : MonoBehaviour
         yRotation += mouseX;
         xRotation -= mouseY;
 
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -85f, 60f);
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
