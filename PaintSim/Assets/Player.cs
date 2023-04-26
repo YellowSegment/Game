ece2634f4d2806b75f910e04c62be2d2d6a14d38
YellowSegment <yellowsegment66@gmail.com>
2023-04-25 20:24:42 -0400

    MOVEMENT
    Added sprinting and crouching as well as some drag so there is no "ice sliding"

--- PaintSim/Assets/Player.cs
+++ PaintSim/Assets/Player.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed;
+    private float moveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
     public float jumpForce;
     float verticalInput;
     float horizontalInput;
@@ -12,14 +14,38 @@ public class Player : MonoBehaviour
     public Transform orientation;
     Vector3 moveDirection;
 
+    public float groundDrag;
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
+
+    public MovementState state;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        startYScale = transform.localScale.y;
     }
     private void Update()
     {
+        SpeedControl();
         MyInput();
+        StateHandler();
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * .5f + .2f, whatIsGround);
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
     }
     private void FixedUpdate()
     {
@@ -31,6 +57,16 @@ public class Player : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+        if (Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
     }
     
     private void MovePlayer()
@@ -39,5 +75,43 @@ public class Player : MonoBehaviour
 
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    public void StateHandler()
+    {
+        if (grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+        else
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+        }
+        if (Input.GetKey(crouchKey))
+        {
+            state = MovementState.crouching;
+            moveSpeed = crouchSpeed;
+        }
+    }
+
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        crouching
+    }
+
 }
 
