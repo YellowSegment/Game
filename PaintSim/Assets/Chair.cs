6fd88d32cc587b09c173e001b9b15e4005346846
YellowSegment <yellowsegment66@gmail.com>
2023-05-08 21:12:35 -0400

    Added UI
    Added UI to the PC and chair to prompt user with some options. Did not fix issue with the chair though

--- PaintSim/Assets/Chair.cs
+++ PaintSim/Assets/Chair.cs
@@ -6,23 +6,37 @@ public class Chair : MonoBehaviour
 {
     public GameObject playerStanding;
     public GameObject playerSitting;
+    public GameObject UItext;
+    public GameObject Desktop;
+    public GameObject LeavePC;
     public Camera standingCamera;
     public Camera sittingCamera;
     public bool interactable;
     public bool sitting;
+    public bool onPC;
 
     void Start()
     {
         // Initialize variables
         interactable = false;
         sitting = false;
+        UItext.SetActive(false);
+        Desktop.SetActive(false);
+        onPC = false;
 
         // Set standing camera to main camera if not assigned
         if (standingCamera == null)
         {
             standingCamera = Camera.main;
         }
+
+        // Set sitting camera to main camera if not assigned
+        if (sittingCamera == null)
+        {
+            sittingCamera = Camera.main;
+        }
     }
+
 
     void OnTriggerStay(Collider other)
     {
@@ -41,50 +55,80 @@ public class Chair : MonoBehaviour
     }
 
     void Update()
-{
-    if (interactable)
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (interactable)
         {
-            playerSitting.SetActive(true);
-            sitting = true;
-            playerStanding.SetActive(false);
-            if (standingCamera != null)
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                standingCamera.enabled = false;
-            }
-            if (sittingCamera != null && sittingCamera.GetComponent<Camera>() != null)
-            {
-                sittingCamera.GetComponent<Camera>().enabled = true;
-            }
-            interactable = false;
+                playerSitting.SetActive(true);
+                sitting = true;
+                playerStanding.SetActive(false);
+                if (standingCamera != null)
+                {
+                    standingCamera.enabled = false;
+                }
+                if (sittingCamera != null && sittingCamera.GetComponent<Camera>() != null)
+                {
+                    sittingCamera.GetComponent<Camera>().enabled = true;
+                }
+                interactable = false;
 
-            // disable the audio listener and camera on the main camera, and enable them on the sitting camera
-            Camera.main.GetComponent<Camera>().enabled = false;
-            if (sittingCamera?.GetComponent<Camera>() != null)
+                // disable the audio listener and camera on the main camera, and enable them on the sitting camera
+                Camera.main.GetComponent<Camera>().enabled = false;
+                if (sittingCamera?.GetComponent<Camera>() != null)
+                {
+                    sittingCamera.GetComponent<Camera>().enabled = true;
+                }
+            }
+        }
+
+        if (sitting)
+        {
+            if (sittingCamera == null)
             {
-                sittingCamera.GetComponent<Camera>().enabled = true;
+                return;
+            }
+
+            if (onPC)
+            {
+                UItext.SetActive(false);
+                LeavePC.SetActive(true);
+            }
+            else
+            {
+                UItext.SetActive(true);
+                LeavePC.SetActive(false);
+            }
+
+            if (Input.GetKeyDown(KeyCode.E) && !onPC)
+            {
+                playerSitting.SetActive(false);
+                playerStanding.SetActive(true);
+                standingCamera.enabled = true;
+                if (sittingCamera != null && sittingCamera.GetComponent<Camera>() != null)
+                {
+                    sittingCamera.GetComponent<Camera>().enabled = false;
+                }
+                sitting = false;
+                UItext.SetActive(false);
+
+                // disable the audio listener and camera on the sitting camera, and enable them on the main camera
+                Camera.main.GetComponent<Camera>().enabled = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Desktop.SetActive(true);
+                onPC = true;
+            }
+
+            if (onPC && Input.GetKeyDown(KeyCode.Escape))
+            {
+                Desktop.SetActive(false);
+                onPC = false;
+                UItext.SetActive(true);
+                LeavePC.SetActive(false);
             }
         }
     }
-
-    if (sitting)
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            playerSitting.SetActive(false);
-            playerStanding.SetActive(true);
-            standingCamera.enabled = true;
-            if (sittingCamera != null && sittingCamera.GetComponent<Camera>() != null)
-            {
-                sittingCamera.GetComponent<Camera>().enabled = false;
-            }
-            sitting = false;
-
-            // disable the audio listener and camera on the sitting camera, and enable them on the main camera
-            Camera.main.GetComponent<Camera>().enabled = true;
-        }
-    }
-}
-
 }
