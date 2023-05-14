836f776860e1332fdebb6cc722ceeb6ac913a62e
YellowSegment <yellowsegment66@gmail.com>
2023-05-14 19:51:46 -0400

    Added button functionality
    Added main menu button functionality. And some other tweaks.

---
+++ PaintSim/Assets/UIScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class UIScript : MonoBehaviour
+{
+    public GameObject SettingsMenu;
+    public GameObject MainMenu;
+    public GameObject Credits;
+    private bool SettingsOpen = false;
+    [SerializeField] private string NewGameLevel = "World1";
+
+
+    public void NewGameButton()
+    {
+        SceneManager.LoadScene(NewGameLevel);
+    }
+
+    public void OpenSettings()
+    {
+        MainMenu.SetActive(false);
+        SettingsMenu.SetActive(true);
+        SettingsOpen = true;
+    }
+
+    public void CloseSettings()
+    {
+        SettingsOpen = false;
+        SettingsMenu.SetActive(false);
+         MainMenu.SetActive(true);
+    }
+   void update()
+   {
+    
+   }
+}
