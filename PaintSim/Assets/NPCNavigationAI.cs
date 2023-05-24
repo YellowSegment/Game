2fa70b632ea30b92050d483d152d227cac0149c4
YellowSegment <yellowsegment66@gmail.com>
2023-05-24 03:01:00 -0400

    Continued NPC Movement
    Added NPC chances to get in store. Need to work on creating a queue and resetting the NPCs after making it to their destination. This is poorly coded at the moment.

--- PaintSim/Assets/NPCNavigationAI.cs
+++ PaintSim/Assets/NPCNavigationAI.cs
@@ -17,17 +17,22 @@ public class NPCNavigationAI : MonoBehaviour
     private float walkPastTime = 30f;
     private bool isWalking;
     private float walkTime;
+    private int startMovement;
+    public GameObject npc;
+    public bool inStore;
 
     // Start is called before the first frame update
     void Start()
     {
         theAgent = GetComponent<NavMeshAgent>();
-        customerChance = 2;
+        customerChance = 5;
         chanceFrequency = 10f;
         time = chanceFrequency;
         PauseAnimator();
         isWalking = false;
         walkTime = walkPastTime;
+        npc.SetActive(false);
+        isPositionOne = false;
     }
 
     // Update is called once per frame
@@ -46,47 +51,71 @@ public class NPCNavigationAI : MonoBehaviour
                 walkTime = walkPastTime;
                 time = chanceFrequency;
                 ChooseDestination();
-                
             }
         }
     }
 
     public void ChooseDestination()
     {
-        walkDirection = Random.Range(0, customerChance);
-        isWalking = true;
-        Debug.Log(walkDirection);
-        unPauseAnimator();
-        if (walkDirection == 0)
+        startMovement = Random.Range(0, customerChance);
+        if (startMovement == 0)
         {
-            if (isPositionOne)
+            npc.SetActive(true);
+            walkDirection = Random.Range(0, customerChance);
+            isWalking = true;
+            Debug.Log(walkDirection);
+            unPauseAnimator();
+            if (walkDirection == 0)
             {
-                theAgent.SetDestination(positionOne.transform.position);
-            }
-            else if (isPositionTwo)
-            {
-                theAgent.SetDestination(positionTwo.transform.position);
-            }
-            else if (isPositionThree)
-            {
-                theAgent.SetDestination(positionThree.transform.position);
-            }
-            else if (isPositionFour)
-            {
-                theAgent.SetDestination(positionFour.transform.position);
-            }
-            else if (isPositionFive)
-            {
-                theAgent.SetDestination(positionFive.transform.position);
+                inStore = true;
+                if (!isPositionOne)
+                {
+                    theAgent.SetDestination(positionOne.transform.position);
+                    isWalking = true;
+                    isPositionOne = true;
+                    walkDirection = -1;
+                }
+                else if (!isPositionTwo)
+                {
+                    theAgent.SetDestination(positionTwo.transform.position);
+                    isWalking = true;
+                    isPositionTwo = true;
+                    walkDirection = -1;
+                }
+                else if (!isPositionThree)
+                {
+                    theAgent.SetDestination(positionThree.transform.position);
+                    isWalking = true;
+                    isPositionThree = true;
+                    walkDirection = -1;
+                }
+                else if (!isPositionFour)
+                {
+                    theAgent.SetDestination(positionFour.transform.position);
+                    isWalking = true;
+                    isPositionFour = true;
+                    walkDirection = -1;
+                }
+                else if (!isPositionFive)
+                {
+                    theAgent.SetDestination(positionFive.transform.position);
+                    isWalking = true;
+                    isPositionFive = true;
+                    walkDirection = -1;
+                }
+                else
+                {
+                    theAgent.SetDestination(walkPastDestination.transform.position);
+                    isWalking = true;
+                    walkDirection = -1;
+                }
             }
             else
             {
                 theAgent.SetDestination(walkPastDestination.transform.position);
+                isWalking = true;
+                walkDirection = -1;
             }
-        }
-        else
-        {
-            theAgent.SetDestination(walkPastDestination.transform.position);
         }
     }
 
