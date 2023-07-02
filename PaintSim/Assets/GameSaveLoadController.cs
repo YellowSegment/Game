fe1809f3363c5cfa021bfb9665db2692d3454a97
YellowSegment <yellowsegment66@gmail.com>
2023-07-01 21:51:41 -0400

    Saving between launches
    Implemented the ability to save and load between launches. There is still more data that needs grabbing as we develop more, but it is in a functional state. I will have to do more with this to load the correct world and load on startup, but there is now saving. (Press G to save to a file, and on restart press H to load from that file)

--- PaintSim/Assets/GameSaveLoadController.cs
+++ PaintSim/Assets/GameSaveLoadController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class GameSaveLoadController : MonoBehaviour
 {
@@ -79,16 +80,78 @@ public class GameSaveLoadController : MonoBehaviour
         paintOptions = paintOrder.GetPaintRange();
 
         Debug.Log("Game Successfully Saved");
+
+        string destination = Application.persistentDataPath + "/save.dat";
+        using (StreamWriter writer = new StreamWriter(destination))
+        {
+            writer.WriteLine(worldNumber);
+            writer.WriteLine(balance);
+            writer.WriteLine(paintCans);
+            writer.WriteLine(paintCansComing);
+            writer.WriteLine(paintCansTotal);
+            writer.WriteLine(redTintCans);
+            writer.WriteLine(redTintCansComing);
+            writer.WriteLine(redTintCansTotal);
+            writer.WriteLine(greenTintCans);
+            writer.WriteLine(greenTintCansComing);
+            writer.WriteLine(greenTintTotal);
+            writer.WriteLine(blueTintCans);
+            writer.WriteLine(blueTintCansComing);
+            writer.WriteLine(blueTintTotal);
+            writer.WriteLine(stirSticks);
+            writer.WriteLine(stirSticksComing);
+            writer.WriteLine(tintAmount[0]);
+            writer.WriteLine(tintAmount[1]);
+            writer.WriteLine(tintAmount[2]);
+            writer.WriteLine(dayNumbers);
+            writer.WriteLine(timeOfDay);
+            writer.WriteLine(paintOptions);
+        }
     }
     public void Load()
     {
-        inventoryManager.LoadData(worldNumber, paintCans, paintCansTotal, redTintCans, redTintCansTotal, greenTintCans, greenTintTotal, blueTintCans, blueTintTotal, stirSticks, tintAmount);
-        balanceController.setBalance(balance);
-        dayEndController.LoadData(paintCansComing, redTintCansComing, greenTintCansComing, blueTintCansComing, stirSticksComing, dayNumbers, timeOfDay);
-        paintOrder.setPaintRange(paintOptions);
-        buttonUI.resetCans();
-        buttonUI.addCans(paintCans);
-        
-        Debug.Log("Game Successfully Loaded");
-    }    
+        string destination = Application.persistentDataPath + "/save.dat";
+
+        if (File.Exists(destination))
+        {
+            using (StreamReader reader = new StreamReader(destination))
+            {
+                worldNumber = int.Parse(reader.ReadLine());
+                balance = double.Parse(reader.ReadLine());
+                paintCans = int.Parse(reader.ReadLine());
+                paintCansComing = int.Parse(reader.ReadLine());
+                paintCansTotal = int.Parse(reader.ReadLine());
+                redTintCans = int.Parse(reader.ReadLine());
+                redTintCansComing = int.Parse(reader.ReadLine());
+                redTintCansTotal = int.Parse(reader.ReadLine());
+                greenTintCans = int.Parse(reader.ReadLine());
+                greenTintCansComing = int.Parse(reader.ReadLine());
+                greenTintTotal = int.Parse(reader.ReadLine());
+                blueTintCans = int.Parse(reader.ReadLine());
+                blueTintCansComing = int.Parse(reader.ReadLine());
+                blueTintTotal = int.Parse(reader.ReadLine());
+                stirSticks = int.Parse(reader.ReadLine());
+                stirSticksComing = int.Parse(reader.ReadLine());
+                tintAmount[0] = int.Parse(reader.ReadLine());
+                tintAmount[1] = int.Parse(reader.ReadLine());
+                tintAmount[2] = int.Parse(reader.ReadLine());
+                dayNumbers = int.Parse(reader.ReadLine());
+                timeOfDay = int.Parse(reader.ReadLine());
+                paintOptions = int.Parse(reader.ReadLine());
+            }
+
+            inventoryManager.LoadData(worldNumber, paintCans, paintCansTotal, redTintCans, redTintCansTotal, greenTintCans, greenTintTotal, blueTintCans, blueTintTotal, stirSticks, tintAmount);
+            balanceController.setBalance(balance);
+            dayEndController.LoadData(paintCansComing, redTintCansComing, greenTintCansComing, blueTintCansComing, stirSticksComing, dayNumbers, timeOfDay);
+            paintOrder.setPaintRange(paintOptions);
+            buttonUI.resetCans();
+            buttonUI.addCans(paintCans);
+
+            Debug.Log("Game Successfully Loaded");
+        }
+        else
+        {
+            Debug.Log("No saved game found");
+        }
+    }
 }
